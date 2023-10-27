13a5abfa718e76ac95430e8d66c3e8cc1d492635
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-28 01:18:02 +0900

    🎨  Change StatelessWidget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
